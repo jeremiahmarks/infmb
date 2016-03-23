56af51d2b740bb9f07732c722c64725139b89c58
Jeremiah Marks <jeremiah@jlmarks.org>
2016-03-22 17:56:46 -0700

    attempting to runserver better

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@
 import os
 from app import create_app, db
 from app.models import User, Role
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -19,7 +19,7 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
 
 @manager.command
 def test():
